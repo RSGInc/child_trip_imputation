ba3120daea9f2821ba2070e2133afa8b2bf74f25
Nick Fournier <nick.fournier@rsginc.com>
2023-06-04 11:07:32 -0700

    added check for missing school trip function

--- child_trip_imputation/imputation.py
+++ child_trip_imputation/imputation.py
@@ -44,44 +44,47 @@ class Imputation(IO):
             hh_persons = persons_df[persons_df.hh_id == hh_id]
             
             # Inner loop over persons in household, adding Household to Person manager 
-            for person_id, person in hh_persons.groupby('person_id'):                
-                                
+            for person_id, person in hh_persons.groupby('person_id'):                                
                 Person = PersonManagerClass(person, Household)
                 person_days = days_df[days_df.person_id == person_id]
                 
                 # Inner-inner loop over days for persons, adding Person to Day manager
                 for day_id, day in person_days.groupby('day_id'):
                     Day = DayManagerClass(day, Person)
-                    day_trips = trips_df[trips_df.day_id == day_id]                    
+                    day_trips = trips_df[trips_df.day_id == day_id]
+                    
+                    # TODO: Create tours at this point instead of going right to day_trips above
+                    # person_day_tours = trips_to_tours()
+                    # for tour_id, tour in person_day_tours.groupby('tour_id'):
+                    #    Tour = TourManagerClass(tour, Day)
+                    #    tour_trips = trips_df[trips_df.tour_id == tour_id]                    
                     
                     # Skip if imputation is required
-                    if not self.is_impute_needed(Day, day_trips):
+                    if not self.missing_school_trip(Day, day_trips):
                         continue                
                         
             
-    def is_impute_needed(self, Day, day_trips):
+    def missing_school_trip(self, Day, day_trips):
         
         assert isinstance(settings.CODES, dict) 
         CHILD_AGE_COL, CHILD_AGE_CODES = get_codes('CHILD_AGE')
         PRESCHOOL_AGE_COL, PRESCHOOL_AGE_CODES = get_codes('PRESCHOOL_AGE')
         PRESCHOOL_TYPE_COL, PRESCHOOL_TYPE_CODES = get_codes('PRESCHOOL_TYPES')
+        SCHOOL_PURPOSES_COL, SCHOOL_PURPOSES_CODES = get_codes('SCHOOL_PURPOSES')
         
         # Skip if person not proxy (is adult)        
         if not Day.Person.data[CHILD_AGE_COL].isin(CHILD_AGE_CODES).iloc[0]:
             return False
-        
-        # Skip if person has outbound school trip already
-        day_trips
-        
-        
-        
-        
+                
         # Skip if person is pre-school age and school_type is not preschool (does not attend preschool)
         is_preschool_age = Day.Person.data[PRESCHOOL_AGE_COL].isin(PRESCHOOL_AGE_CODES).iloc[0]
         is_in_preschool = Day.Person.data[PRESCHOOL_TYPE_COL].isin(PRESCHOOL_TYPE_CODES).iloc[0]
         if is_preschool_age and not is_in_preschool:
             return False
         
+        # Skip if person (child) already has school destination
+        if not day_trips[SCHOOL_PURPOSES_COL].isin(SCHOOL_PURPOSES_CODES).iloc[0].any():
+            return False               
         
         return True 
         
